cb8ea470f97b5bed6b835ba451ea3ea324851014
Chungyuen Li <lizoyu1@gmail.com>
2017-06-19 17:36:23 -0500

    add read data function

---
+++ parse.py
@@ -0,0 +1,27 @@
+from os import listdir
+from cv2 import imread, imshow, waitKey
+
+def parse(path):
+	'''
+	read all images in the folder(path)
+	input:
+		- path: the directory of the images
+	output:
+		- imgs: list of images
+	'''
+	imgs = []
+	for imgname in listdir(path):
+		img = imread(str(path+'/'+imgname), 0)
+		imgs.append(img.reshape([img.size,]))
+	return imgs
+
+def tester():
+	imgs = parse('./data/2d_images')
+	part = imgs[0:3]
+	import numpy as np
+	part = np.array(part,np.uint8)
+	print part
+	print part.shape
+	print len(imgs)
+
+#tester()
