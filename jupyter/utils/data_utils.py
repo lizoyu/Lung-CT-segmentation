5c075aa2beb5f6f5ba4ed43a1945d8e2a5c6d96b
lizoyu <lizoyu1@gmail.com>
2017-11-06 09:36:18 -0600

    memory exhausted

--- jupyter/utils/data_utils.py
+++ jupyter/utils/data_utils.py
@@ -1,4 +1,5 @@
 from os import listdir
+import numpy as np
 from cv2 import imread, imshow, waitKey
 
 def parse(path):
@@ -12,24 +13,24 @@ def parse(path):
 	imgs = []
 	for imgname in listdir(path):
 		img = imread(str(path+'/'+imgname), 0)
-		imgs.append(img.reshape([img.size,]))
+		imgs.append(img)
 	return imgs
 
 def read_data():
 	'''
 	read train and test data and masks.
 	'''
-	x_train = parse('./data/2d_images')
-	y_train = parse('./data/2d_masks')
+	x_train = parse('../data/2d_images')
+	y_train = parse('../data/2d_masks')
 	return np.array(x_train), np.array(y_train)
 
+'''
 def tester():
 	imgs = parse('./data/2d_images')
 	part = imgs[0:3]
-	import numpy as np
 	part = np.array(part,np.uint8)
 	print part
 	print part.shape
 	print len(imgs)
-
+'''
 #tester()
